a4eaeb85bc04dc1a2c04759044a93f7d21327948
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-05-27 15:10:32 +0900

    ✨[feat] 적 객체 기초 움직임 구현
    랜덤 위치 지정 후 움직이는 시스템 구현

--- Assets/01.Scripts/Entity/Enemy/Enemy.cs
+++ Assets/01.Scripts/Entity/Enemy/Enemy.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 /// 공격시 - 원거리, 근거리로 나눈다면 근거리 적, 보스, 각각 Enemy 상속 받아서 사용
 /// IDamageble, IMoveable, IAttackable
 /// </summary>
-public abstract class Enemy : Entity
+public abstract class Enemy : Entity, IDamagable, IMoveable
 {
-    
+    public abstract void TakeDamage();
+
+    public abstract void Move();
 }
