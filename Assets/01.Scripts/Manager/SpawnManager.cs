040c68d2fa9709f89c2847ea984d91c501cbf653
고윤아 <0205ria@gmail.com>
2025-05-27 14:41:21 +0900

    🔀[merge] branch 'dev' into featBuildSystem

--- Assets/01.Scripts/Manager/SpawnManager.cs
+++ Assets/01.Scripts/Manager/SpawnManager.cs
@@ -1,11 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+
 /// <summary>
 /// PoolManager를 Get. Pool을 활용해 Object 생성 처리 담당
 /// </summary>
+
 public class SpawnManager : MonoBehaviour
 {
+    private static SpawnManager instance;
+
+    private Dictionary<string, object> pools = new Dictionary<string, object>();
+    
+    public static SpawnManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject("SpawnManager").AddComponent<SpawnManager>();
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        //CreatePool<Item>("Item", Item);
+        //CreatePool<Enemy>("Enemy",Enemy);
+    }
+
+    private void CreatePool<T>(string key, T prefab) where T : Component
+    {
+        GameObject poolParent = new GameObject($"{key}Pool");
+        poolParent.transform.SetParent(transform);
+        
+        PoolManager<T> pool = new PoolManager<T>(prefab, poolParent.transform);
+        pools[key] = pool;
+    }
+
+    private PoolManager<T> GetPool<T>(string key) where T : Component
+    {
+        return pools[key] as PoolManager<T>;
+    }
+
+    public T GetObject<T>(string key, T Prefab) where T : Component
+    {
+        return GetPool<T>(key).Get();
+    }
+
+    public void ReleaseObject<T>(string key, T obj) where T : Component
+    {
+        GetPool<T>(key).Release(obj);
+    }
+    
     // 필드
     // ItemPool<Item>
     // EnemyPool<Enemy>
