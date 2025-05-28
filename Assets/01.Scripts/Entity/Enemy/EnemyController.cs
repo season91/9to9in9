4b66eb2e8d241317d8b7fdceb5218f9e7fa9d49a
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-05-28 10:55:04 +0900

    🔨[fix] ai 끼임 문제, 길찾기 불가 문제
    1. Environment 프리펩의 객체들의 navMesh Obstacle 컴포넌트 삭제
    2. Bake 설정 -> radius 조정
    3. attack 애니메이션 exit time 추가
    
    related to #20

--- Assets/01.Scripts/Entity/Enemy/EnemyController.cs
+++ Assets/01.Scripts/Entity/Enemy/EnemyController.cs
@@ -1,17 +1,22 @@
+using System;
 using UnityEngine;
 /// <summary>
 /// 움직임을 적 종류마다 다르게 구현
 /// </summary>
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public IDamagable damagable;
+    public IAttackAble attackable;
+    public IMoveable moveable;
+
+    private void Start()
     {
-        
+        damagable = GetComponent<IDamagable>();
+        attackable = GetComponent<IAttackAble>();
+        moveable = GetComponent<IMoveable>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
