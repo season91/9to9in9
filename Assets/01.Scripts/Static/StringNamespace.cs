e606d79268a174b8778e919f34d20efc5fd052b2
season91 <choayoung91@naver.com>
2025-05-28 20:54:28 +0900

    🔀[merge] 좀비 병합
    [merge] 좀비 병합

--- Assets/01.Scripts/Static/StringNamespace.cs
+++ Assets/01.Scripts/Static/StringNamespace.cs
@@ -22,8 +22,6 @@ public static class StringAddressable
     public const string StartScene = "StartScene";
 }
 
-// 아이템 이름 싹다 넣어주기
-
 public static class StringItemOftenUse
 {
     public const string Rock = "Rock";
@@ -31,4 +29,34 @@ public static class StringItemOftenUse
     //----오브젝트 풀 테스트용 키 밸류
     
     public static readonly string[] Keys = { Rock, Wood };
+}
+
+public static class StringItemName
+{
+    public const string Meat = "meat";
+    public const string Apple = "apple";
+    public const string MeatCooked = "meat-cooked";
+    public const string Orange = "orange";
+    public const string Pear = "pear";
+    public const string Mushroom = "mushroom";
+    public const string ToolAxe = "tool-axe";
+    public const string ToolPickaxe = "tool-pickaxe";
+    public const string Helmet = "helmet";
+    public const string ArmorChest = "armor-chest";
+    public const string ArmorLeg = "armor-leg";
+    public const string Boots = "boots";
+    public const string Shield = "shield";
+    public const string Sword = "sword";
+    public const string Hammer = "hammer";
+    public const string Stone = "stone";
+    public const string Wood = "wood";
+    public const string Iron = "iron";
+    public const string IronBar = "iron-bar";
+    public const string WoodenWall = "wooden-wall";
+    public const string WoodenFloor = "wooden-floor";
+    public const string MetalWall = "metal-wall";
+    public const string Workbench = "workbench";
+    public const string Campfire = "campfire";
+    public const string Smelter = "smelter";
+    public const string Anvil = "anvil";
 }
