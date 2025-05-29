8dee6cade90247e4ce51111dc911fb2319f69129
Youngim <immy0815@users.noreply.github.com>
2025-05-29 16:39:09 +0900

    🔨[fix] DebugWindow에 AddItemToInventory 추가

--- Assets/98.Tests/DebugWindow.cs
+++ Assets/98.Tests/DebugWindow.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.InputSystem;
@@ -14,11 +16,57 @@ public class DebugWindow : EditorWindow
 
     void OnGUI()
     {
-        if (GUILayout.Button("AddItemToInventory"))
+        if (GUILayout.Button("AddItemsToInventory"))
         {
-            // ResourceManager.Instance.GetResource<ItemData>()
-            // CharacterManager.Player.inventoryController.AddItem();
-            // CharacterManager.Player.inventoryController.AddItem();
+            // 아이템 19개 추가 => Consumable, Equipable, Resource
+            List<string> addressList = new List<string>();
+
+            FieldInfo[] fields;
+            
+            // Field의 public static으로 선언된 변수들을 가져옴
+            fields = typeof(StringAdrItemDataConsumable)
+                .GetFields(BindingFlags.Public | BindingFlags.Static);
+            foreach (FieldInfo field in fields)
+            {
+                // IsLiteral => const로 선언된지 확인
+                // FieldType == typeof(string) => 자료형이 string인 필드인지 확인
+                if (field.IsLiteral && field.FieldType == typeof(string))
+                {
+                    // 해당 const string 필드의 값을 가져옴
+                    // const를 가져오기 위해 GetRawConstantValue를 사용해야함
+                    string value = field.GetRawConstantValue() as string;
+                    addressList.Add(value);
+                }
+            }
+            
+            fields = typeof(StringAdrItemDataEquipable)
+                .GetFields(BindingFlags.Public | BindingFlags.Static);
+            foreach (FieldInfo field in fields)
+            {
+                if (field.IsLiteral && field.FieldType == typeof(string))
+                {
+                    string value = field.GetRawConstantValue() as string;
+                    addressList.Add(value);
+                }
+            }
+            
+            fields = typeof(StringAdrItemDataResource)
+                .GetFields(BindingFlags.Public | BindingFlags.Static);
+            foreach (FieldInfo field in fields)
+            {
+                if (field.IsLiteral && field.FieldType == typeof(string))
+                {
+                    string value = field.GetRawConstantValue() as string;
+                    addressList.Add(value);
+                }
+            }
+
+            foreach (var address in addressList)
+            {
+                var item = ResourceManager.Instance.GetResource<ItemData>(address);
+                CharacterManager.Player.inventoryController.AddItem(item);
+            }
+            
         }
     }
 }
