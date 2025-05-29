2521450670163f47a3404b8365bfdc6396b35f3a
season91 <choayoung91@naver.com>
2025-05-29 23:20:30 +0900

    ✨[feat] GameManager 싱글톤 처리
    GameManager 싱글톤 처리

--- Assets/01.Scripts/Manager/GameManager.cs
+++ Assets/01.Scripts/Manager/GameManager.cs
@@ -1,25 +1,53 @@
+using System;
 using UnityEngine;
 /// <summary>
 /// [전역] 게임 진행
 /// GameStart, GameOver
 /// </summary>
-public static class GameManager
+public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                return null;
+            return instance;
+        }
+    }
+    
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+
+    private void Start()
+    {
+        
+    }
+
     // test code
-    public static void InitMainScene()
+    public void InitMainScene()
     {
         Debug.Log("MainScene 초기화 시작");
         ResourceSpawn();
     }
 
-    public static void ResourceSpawn()
+    public void ResourceSpawn()
     {
         if (SpawnManager.Instance == null)
         {
             Debug.LogWarning("SpawnManager 인스턴스가 null입니다!");
             return;
         }
-
+        
+        Debug.Log("ResourceSpawn 시작!!");
         SpawnManager.Instance.GetObject(StringItemName.Tree);
     }
 
