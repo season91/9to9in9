06b96591a45d2acb4e86274de2dfdd23e1061082
Hwang Duckbae <87626940+Neronem@users.noreply.github.com>
2025-05-30 17:57:28 +0900

    ✨[feat] NPC 대화
    * NPC에게 접근 후 Enter 입력 시 대화 시작
    * Enter로 다음 대화 이어나감

--- Assets/01.Scripts/Manager/DialogueManager.cs
+++ Assets/01.Scripts/Manager/DialogueManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using TMPro;
@@ -5,75 +6,118 @@ using DG.Tweening;
 
 public class DialogueManager : MonoBehaviour
 {
-    public static DialogueManager Instance; // 싱글톤
+    public static DialogueManager instance;
+    
+    // private CanvasGroup dialogueCanvasGroup; // 알파 조절을 위한 CanvasGroup
+    // public TextMeshProUGUI dialogueText;
+    public string[] dialogueLines;
 
-    public GameObject dialogueUI;
-    public TextMeshProUGUI dialogueText;
+    private int currentLine = 0;
+    private bool isPlayerInRange = false;
+    private bool isTyping = false;
+    private Tween typingTween;
 
-    private string[] currentLines;
-    private int currentLine;
-    private bool isTyping;
-    private Coroutine typingCoroutine;
+    private NPCDialogue currentNPCDialogue; // 현재 말 걸고 있는 NPC
 
     private void Awake()
     {
-        if (Instance == null) Instance = this;
-        else Destroy(gameObject);
+        if (instance == null)
+        {
+            instance = this;
+        }
     }
 
     private void Start()
     {
-        dialogueUI.SetActive(false);
+    //     dialogueCanvasGroup = GameObject.Find("DialogueCanvas").GetComponent<CanvasGroup>();
+    //     dialogueText = GameObject.Find("DialogueCanvas/Group_Dialogue/Tmp_Dialogue").GetComponent<TextMeshProUGUI>();
+    //     
+        dialogueCanvasGroup.alpha = 0f;
+        dialogueCanvasGroup.interactable = false;
+        dialogueCanvasGroup.blocksRaycasts = false;
     }
 
     private void Update()
     {
-        if (dialogueUI.activeSelf && Input.GetKeyDown(KeyCode.Return))
+        if (currentNPCDialogue != null && Input.GetKeyDown(KeyCode.Return))
         {
-            if (isTyping)
+            if (dialogueCanvasGroup.alpha == 0f)
             {
-                StopCoroutine(typingCoroutine);
-                dialogueText.text = currentLines[currentLine];
-                isTyping = false;
+                ShowDialogue();
             }
             else
             {
-                currentLine++;
-                if (currentLine < currentLines.Length)
+                if (isTyping)
                 {
-                    typingCoroutine = StartCoroutine(TypeLine(currentLines[currentLine]));
+                    typingTween.Kill();
+                    dialogueText.maxVisibleCharacters = dialogueLines[currentLine].Length;
+                    isTyping = false;
                 }
                 else
                 {
-                    EndDialogue();
+                    NextLine();
                 }
             }
         }
     }
 
-    public void StartDialogue(string[] lines)
+
+    private void ShowDialogue()
     {
-        dialogueUI.SetActive(true);
-        currentLines = lines;
         currentLine = 0;
-        typingCoroutine = StartCoroutine(TypeLine(currentLines[currentLine]));
+        dialogueCanvasGroup.DOFade(1f, 0.5f).OnComplete(() =>
+        {
+            dialogueCanvasGroup.interactable = true;
+            dialogueCanvasGroup.blocksRaycasts = true;
+            PlayTypingEffect(dialogueLines[currentLine]);
+        });
     }
 
-    private void EndDialogue()
+    private void NextLine()
     {
-        dialogueUI.SetActive(false);
-        dialogueText.text = "";
+        currentLine++;
+        if (currentLine < dialogueLines.Length)
+        {
+            PlayTypingEffect(dialogueLines[currentLine]);
+        }
+        else
+        {
+            EndDialogue();
+        }
     }
 
-    private IEnumerator TypeLine(string line)
+    public void EndDialogue()
+    {
+        dialogueCanvasGroup.DOFade(0f, 0.5f).OnComplete(() =>
+        {
+            dialogueCanvasGroup.interactable = false;
+            dialogueCanvasGroup.blocksRaycasts = false;
+            dialogueText.text = "";
+            currentNPCDialogue = null;
+        });
+    }
+
+    private void PlayTypingEffect(string line)
     {
         isTyping = true;
-        dialogueText.text = "";
-        foreach (char c in line)
+        dialogueText.text = line;
+        dialogueText.maxVisibleCharacters = 0;
+
+        typingTween = DOTween.To(
+            () => dialogueText.maxVisibleCharacters,
+            x => dialogueText.maxVisibleCharacters = x,
+            line.Length,
+            0.05f * line.Length
+        ).OnComplete(() =>
         {
-            dialogueText.text += c;
-            yield return new WaitForSeconds(0.03f);
-        }
-        isTyping = false;
+            isTyping = false;
+        });
+    }
+
+    public void StartDialogue(NPCDialogue npcDialogue)
+    {
+        currentNPCDialogue = npcDialogue;
+        dialogueLines = npcDialogue.dialogueLines;
+        currentLine = 0;
     }
 }
