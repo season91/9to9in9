f440efe02a40023910de767bf972bf2c4944c62d
고윤아 <0205ria@gmail.com>
2025-05-30 10:21:51 +0900

    ✨[feat] 무기 장착 시 화면에 보임

---
+++ Assets/01.Scripts/Entity/Player/PlayerEquip.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerEquip : MonoBehaviour
+{
+    [SerializeField] private GameObject curEquip;
+    [SerializeField] private Transform equipParent;
+
+    private void Reset()
+    {
+        equipParent = GameObject.Find("Equip Camera").transform;
+    }
+    
+    
+    public void Equip(ItemData item)
+    {
+        UnEquip();
+        curEquip = Instantiate(item.prefab, equipParent);
+        
+        Debug.Log($"[ItemObject-{name}] 장착됨 !");
+    }
+
+    
+    public void UnEquip()
+    {
+        if (!curEquip) return;
+        
+        Destroy(curEquip.gameObject);
+        curEquip = null;
+        Debug.Log($"[ItemObject-{name}] 해제됨 !");
+    }
+    
+    /// <summary>
+    /// 오른손에 든 거 사용하기
+    /// 타입에 따라 switch (공격 / 채집)
+    /// </summary>
+    /// <param name="item">EquipableItemData</param>
+    
+    private void UseEquipableItem(EquipableItemData item)
+    {
+        switch (item.equipType)
+        {
+            case EquipType.Weapon:
+                // TODO: 휘두르기
+                break;
+            
+            case EquipType.GatheringTool:
+                // TODO: 채집하기
+                break;
+        }
+    }
+}
