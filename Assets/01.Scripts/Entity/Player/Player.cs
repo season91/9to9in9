59327ae4a837039efd7b532332f961ac84fd74e0
고윤아 <0205ria@gmail.com>
2025-05-31 11:57:11 +0900

    ✨[feat] 배고픔 / 스태미나 시스템
    * Player.cs Update 내부에 1초마다 실행되도록 넣어놨는데 혹시 모르니 확인 부탁드립니다..!
    
    *

--- Assets/01.Scripts/Entity/Player/Player.cs
+++ Assets/01.Scripts/Entity/Player/Player.cs
@@ -1,4 +1,5 @@
 
+using System;
 using UnityEngine;
 
 /// <summary>
@@ -14,6 +15,10 @@ public class Player : Entity, IDamagable
     
     [SerializeField] private StatProfile statProfile;
     
+    private float timer;
+    private float hungerPassive;
+    private float staminaPassive;
+    
     private void Awake()
     {
         //getcomponent에서 null 반환 시 addcomponent할 것
@@ -23,6 +28,9 @@ public class Player : Entity, IDamagable
         statHandler = GetComponent<StatHandler>();
         interactionHandler = GetComponent<PlayerInteractionHandler>();
         equipHandler = GetComponent<PlayerEquipHandler>();
+
+        hungerPassive = CharacterManager.Player.statHandler.GetPassive(StatType.Hunger);
+        staminaPassive = CharacterManager.Player.statHandler.GetPassive(StatType.Stamina);
         
         // null체크
         if (controller == null) Debug.LogError("Player Controller not found");
@@ -40,13 +48,35 @@ public class Player : Entity, IDamagable
         SpawnManager temp = SpawnManager.Instance;
     }
 
+    private void Update()
+    {
+        // 배고픔 수치 감소
+        timer += Time.deltaTime;
+
+        if (!(timer >= 1f)) return;
+        
+        
+        timer = 0f;
+        statHandler.Modify(StatType.Hunger, hungerPassive);
+        
+        bool empty = statHandler.IsEmpty(StatType.Hunger);
+        
+        // 스태미나 수치 증가
+        if (empty) return; // 배고픔 0이면 스태미나 리젠 X
+        statHandler.Modify(StatType.Stamina, staminaPassive);
+        
+        // 1초마다 호출되니까 괜찮을까요... 아니면 수정할게요...
+        UIManager.Instance.UpdateStatUI(StatType.Hunger);
+        UIManager.Instance.UpdateStatUI(StatType.Stamina);
+    }
+    
     public void TakeDamage(float damage)
     {
         statHandler.Modify(StatType.Health, -damage);
         SoundManager.Instance.PlayRandomSfx(SfxType.Hit);
         UIManager.Instance.UpdateStatUI(StatType.Health);
     }
-
+    
     public void Die()
     {
         Debug.Log("플레이어 사망!");
